278cf2b0904115ccc66aff394b05a8242ccafd2d
sanjai <sanjaimass20@gmail.com>
2023-07-13 12:13:07 +0530

    python assignment

--- D11-20230707/assignment/p19.py
+++ D11-20230707/assignment/p19.py
@@ -1,16 +1,18 @@
-print("im thining of a number between 1-100. you have 7 guesses.")
+print("i'm thinking of a number between 1-100. you have 7 guesses.")
 
+import random
+a=random.randint(1,100)
+tries=1
 while True:
-    guess1=int(input("guess#1"))
-    guess2=int(input("guess#2"))
-    guess3=int(input("guess#3"))
-    guess4=int(input("guess#4"))
-    guess5=int(input("guess#5"))
-    guess6=int(input("guess#6"))
-    guess7=int(input("guess#7"))
-     
-    a=(1,100+1)
-    if guess1==a:
+    guess=int(input(f"guess # {tries}:\n"))
+    tries=tries+1
+    if tries>7:
+        print("sorry, you didn't guess it in 7 tries. you lose" )     
+        break  
+    elif guess==a:
         print("you guessed it! what are the odds?!?")
-    
-    
+        break
+    elif guess >a:
+        print("sorry, you are too high.")
+    elif guess <a:
+        print("sorry, you are too low.")
