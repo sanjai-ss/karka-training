80e5d40e1a9711e33e8e7a37311b89a96e5b94c1
sanjai <sanjaimass20@gmail.com>
2023-07-25 19:45:43 +0530

    karka training

--- D24-20230725/practice/date.py
+++ D24-20230725/practice/date.py
@@ -8,13 +8,16 @@
 # curr_time=curr_time.minute
 # print(curr_time)
 
-
 from datetime import datetime
 curr_datetime=datetime(2020,5,6,11,12,13)
-# curr_datetime=datetime.today().hour
+# curr_datetime=datetime.today()
 # curr_datetime=curr_datetime.month
 # curr_datetime=datetime.today()
-# print(curr_datetime)
-curr_datetime=datetime.now()
+# curr_datetime=datetime.now()
 curr_datetime=curr_datetime.strftime("%D")
 print(curr_datetime)
+
+# from datetime import datetime
+# import pytz
+# curr_timezone=datetime.now(pytz.timezone("Asia/kolkata"))
+# print(curr_timezone)
